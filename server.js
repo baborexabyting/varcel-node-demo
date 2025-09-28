c8a5f9cde9fd728fe8572006e959d5c3b2e903c6
Babor <shofiullah@exabyting.com>
2025-09-28 23:45:05 +0600

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 8080;
+
+// Root route (for testing)
+app.get("/", (req, res) => {
+  res.send("🚀 Express app is running on Vercel");
+});
+
+// Slash command route
+app.post("/api/slash", (req, res) => {
+  const msg = `https://meet.google.com/ikw-ghpa-yqb`;
+
+  res.json({
+    response_type: "in_channel",
+    text: `👋 Hello <@${msg}>! This is your predefined message 🎉`
+  });
+});
+
+app.listen(port, () => {
+  `Server started on port ${port}`;
+});
